85a1c661f52e680eacd1aaeace32eadadce4d71e
OlegKryuchkov <kryuchkov.oleg@gmail.com>
2023-10-26 12:46:07 +0300

    ready to check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import style from '../components/App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Albir Rojas', number: '667-00-76' },
+      { id: 'id-2', name: 'Asia Voronova', number: '865-65-43' },
+      { id: 'id-3', name: 'Oksana Sidorska', number: '532-09-21' },
+      { id: 'id-4', name: 'Sandra Gutierrez', number: '321-43-09' },
+    ],
+    filter: '',
+  };
+
+  handleAddNewContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  handleDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    const contactsName = contacts.map(contact => contact.name);
+
+    return (
+      <div>
+        <h1 className={style.title}>Phonebook</h1>
+        <ContactForm
+          onSubmit={this.handleAddNewContact}
+          contactsName={contactsName}
+        />
+
+        <h2 className={style.title}>Contacts</h2>
+        <div className={style.contact_list_container}>
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList
+            visibleContacts={visibleContacts}
+            onDeleteContact={this.handleDeleteContact}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import style from './Filter.module.css';
+
+const Filter = ({ value, onChange }) => (
+  <div className={style.searchContainer}> {/* Додаємо обгортку для центрування */}
+    <label className={style.search}>
+      <input
+        type="text"
+        name="filter"
+        placeholder=" "
+        className={style.inputName}
+        title="Enter search name"
+        onChange={onChange}
+        value={value}
+      />
+    </label>
+  </div>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
